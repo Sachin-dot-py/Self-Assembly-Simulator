38a7106a3b4a43eb5e57f8e842c616b2472749a4
Sachin Ramanathan <sachinr.exe@gmail.com>
2024-11-19 21:44:15 -0800

    Enhance NavBar

--- frontend/app/components/Navigation.js
+++ frontend/app/components/Navigation.js
@@ -1,6 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { FaHome, FaAtom, FaFlask } from 'react-icons/fa'; // Importing icons from react-icons
 
 function Navigation() {
   return (
@@ -18,10 +19,9 @@ function Navigation() {
               Materials Marvels
           </Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/ionic-bonding">Ionic Bonding</Nav.Link>
-            <Nav.Link href="/gold-nanoparticles">Gold Nanoparticles</Nav.Link>
-            {/* <Nav.Link href="/visualization?visualId=40490db3-9ddc-4978-a7c6-0505e00beb30">Example Visualization</Nav.Link> */}
+            <Nav.Link href="/" style={{ display: 'flex', alignItems: 'center' }}><FaHome style={{ marginRight: '8px', verticalAlign: 'middle' }} /> Home</Nav.Link>
+            <Nav.Link href="/ionic-bonding" style={{ display: 'flex', alignItems: 'center' }}><FaAtom style={{ marginRight: '8px', verticalAlign: 'middle' }} /> Ionic Bonding</Nav.Link>
+            <Nav.Link href="/gold-nanoparticles" style={{ display: 'flex', alignItems: 'center' }}><FaFlask style={{ marginRight: '8px', verticalAlign: 'middle' }} /> Gold Nanoparticles</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
