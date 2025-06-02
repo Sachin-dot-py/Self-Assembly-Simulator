9ef24d489dee739c54b549dd52bba14b3c7b8b96
Sachin Ramanathan <sachinr.exe@gmail.com>
2025-06-02 15:55:41 -0700

    Add Google Analytics

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,11 +9,12 @@ export const metadata = {
   title: "Self-Assembly Simulator",
   description: "A web app to simulate Self-Assembly of particles.",
 };
-
+ 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <GoogleAnalytics gaId="G-QFQ8128E4K" />
     </html>
   );
 }
